18b15f0353cd52d8e16dc666b06abb87c5a5cbb9
Axel Lopez <axeluriellopezledesma@gmail.com>
2025-09-08 20:36:53 -0300

    Initial commit

---
+++ src/grupo3_tp4/Alumno.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupo3_tp4;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Alumno {
+    
+    protected int legajo;
+    protected String apellido;
+    protected String nombre;
+    
+     private HashSet<Materia> listamateria=new HashSet();
+
+    public Alumno(int legajo, String apellido, String nombre) {
+        this.legajo = legajo;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+    
+    
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+    public void agregarmateria(Materia m){
+        
+        listamateria.add(m);
+             
+    }
+    
+    public int cantidadMaterias(){
+        
+        return listamateria.size();
+         
+        
+    }    
+    
+    
+}
---
+++ src/grupo3_tp4/Materia.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupo3_tp4;
+
+/**
+ *
+ * @author Adrian
+ */
+public class Materia {
+    
+    protected int idMateria;
+    protected String nombre;
+    protected int año;
+
+    public Materia(int idMateria, String nombre, int año) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.año = año;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + this.idMateria;
+        return hash;
+    }
+
+    @Override
+public boolean equals(Object obj) {
+   
+    if (this == obj) {
+        return true;
+    }
+
+    
+    if (obj == null) {
+        return false;
+    }
+
+    
+    if (!(obj instanceof Materia)) {
+        return false;
+    }
+
+    
+    Materia otra = (Materia) obj;
+
+    // Comparo por idMateria 
+    return this.idMateria == otra.idMateria;
+}
+    
+    
+}
